28766c05b947a1cef2a0104ef2507837b50db37f
robin359 <robinwahl1997@gmail.com>
2020-03-09 12:49:08 +0100

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.write("<h2>Hello World</h2>")
+alert("Thank you for visiting!")
+alert("That's all I wanted to say.")
+
+document.write("<h3 style='color:blue;'>Testscript</h3>")
+
+var phrase = "Robin"
+
+document.write( phrase );
+
+phrase = "Choice"
+
+document.write( phrase );
