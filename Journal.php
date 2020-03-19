766b5afe2bc9b02f25fdda7c3e39df1379bb5586
robin359 <58033638+robin359@users.noreply.github.com>
2020-03-19 23:53:07 +0100

    Update and rename Journal to Journal.php

---
+++ Journal.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hybrider Krieg</title>
+<script src="script.js"></script>
+</head>
+<body>
+<h1>Hybrider Krieg</h1>
+<h2>Und was er verändert</h2>
+<p></p>
+<p>The sense of this website is first of all sharing experiences. So far I kept myself busy with understanding the world we live in. on this way I leasrned a lot of different things, but most important:
+     On this way I made a lot of mistakes. Mistakes I shouldn't have made if I had a guide or a mentor, that accompanied on my way, helped and inspired me.</p>
+<p>So my first goal is to give you advice on how to tackle a new project no matter if it's learning to code, changing habits or playing an instrument. I will be there for you because I went through all of this.</p>
+</body>
+</html>
